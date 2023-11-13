8c31487c5c495dde6a1cb2912c0fc6e21a07ec1e
Benjamin Marty <martyben30@gmail.com>
2023-11-13 16:50:38 +0100

    Adding noise to data - OK

--- add_background_noise.py
+++ add_background_noise.py
@@ -1,9 +1,12 @@
 import os
-from librosa import load, resample
 import numpy as np
 import random
 import soundfile as sf
-from modules import SNR_dB, power
+from modules import SNRin_dB, power
+
+import librosa
+# from librosa import load, resample
+from pathlib import Path
 
 def add_background_noise(audio_path, noise_path, output_folder, snr_level=10):
     """
@@ -15,53 +18,43 @@ def add_background_noise(audio_path, noise_path, output_folder, snr_level=10):
     :param snr_level: Niveau de rapport signal/bruit souhaité.
     """
     # Charger les fichiers audio
-    y, sr = load(audio_path, sr=None)
-    originalNoise, srNoise = load(noise_path, sr=None)
+    y, sr = librosa.load(audio_path, sr=None)
+    originalNoise, srNoise = librosa.load(noise_path, sr=None)
 
     if sr != srNoise:
-        originalNoise = resample(originalNoise, orig_sr=srNoise, target_sr=sr)
+        originalNoise = librosa.resample(originalNoise, orig_sr=srNoise, target_sr=sr)
+
+    # TODO Faire 4 secondes de signal
+    if len(y) < 4 * sr:
+        yResized = np.zeros(4*sr)
+        yResized[:len(y)] = y
+    
+    else:
+        yResized = y[:4*sr]
 
     # Sélection puis somme de deux parties du bruit cafet de la taille du fichier audio y
-    n1 = random.randint(0, len(originalNoise) - len(y))
-    n2 = random.randint(0, len(originalNoise) - len(y))
-    noise1 = originalNoise[n1:n1+len(y)]
-    noise2 = originalNoise[n2:n2+len(y)]
+    n1 = random.randint(0, len(originalNoise) - len(yResized))
+    n2 = random.randint(0, len(originalNoise) - len(yResized))
+    noise1 = originalNoise[n1:n1+len(yResized)]
+    noise2 = originalNoise[n2:n2+len(yResized)]
     
     noise = noise1 + noise2
 
 
     # Calculer la puissance du signal et du bruit
-    y_power = power(y)
+    y_power = power(yResized)
     noise_power = power(noise)
 
     # Calculer le facteur de mise à l'échelle pour atteindre le niveau de SNR désiré
     scale_factor = (y_power / noise_power) * (10 ** (-snr_level / 10))
 
     # Ajouter le bruit au signal d'origine
-    combined_signal = y + noise * np.sqrt(scale_factor)
+    combined_signal = yResized + noise * np.sqrt(scale_factor)
 
     # Enregistrer le nouveau fichier audio
     output_path = os.path.join(output_folder, os.path.basename(audio_path))
     sf.write(output_path, combined_signal, sr)
 
     print(f"Le fichier audio modifié a été enregistré sous : {output_path}")
-    print(SNR_dB(y, np.sqrt(scale_factor)*noise))
+    print(SNRin_dB(yResized, np.sqrt(scale_factor)*noise))
 
-
-# Spécifier les chemins des fichiers et dossiers
-audio_folder = r"C:\Users\torre\Documents\Sicom 3A\Projet_Parole_Audio_SICOM_3A\raw_data"  # chemin du dossier contenant vos fichiers flac
-noise_file = "C:\\Users\\torre\\Documents\\Sicom 3A\\Projet_Parole_Audio_SICOM_3A\\babble_16k.wav"  # chemin du fichier de bruit
-output_folder = "C:\\Users\\torre\\Documents\\Sicom 3A\\Projet_Parole_Audio_SICOM_3A\\noised_data"  # dossier de sortie pour les fichiers audio bruités
-
-# Créer le dossier de sortie s'il n'existe pas
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-
-# Traiter tous les fichiers .flac dans le dossier
-for file in os.listdir(audio_folder):
-    if file.endswith('.flac'):
-        # Chemin complet du fichier audio
-        full_audio_path = os.path.join(audio_folder, file)
-
-        # Appeler la fonction pour ajouter un bruit de fond
-        add_background_noise(full_audio_path, noise_file, output_folder, snr_level = random.randint(-10,10))
---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+
+from pathlib import Path
+from add_background_noise import add_background_noise
+
+
+def noise_data():
+    audio_folder = Path.cwd() / "data" / "raw_data" # chemin du dossier contenant les fichiers flac
+    noise_file = Path.cwd() / "data" / "babble_16k.wav"  # chemin du fichier de bruit
+    output_folder = Path.cwd() / "data" / "noised_data"  # dossier de sortie pour les fichiers audio bruités
+
+    # Créer le dossier de sortie s'il n'existe pas
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+        for i in range(-10, 11):
+            os.makedirs(output_folder / f"{i}")
+
+
+    # Traiter tous les fichiers .flac dans le dossier
+    i=0
+    for file in os.listdir(audio_folder):
+        if file.endswith('.flac'):
+
+            SNRvalue = -10 + i%21
+            full_audio_path = audio_folder / file
+            full_output_folder_path = output_folder / f"{SNRvalue}"
+            
+            # print(full_output_folder_path)
+
+            # Appeler la fonction pour ajouter un bruit de fond
+            add_background_noise(full_audio_path, noise_file, full_output_folder_path, snr_level = SNRvalue)
+        
+        i += 1
+
+
+
+if __name__ == '__main__':
+    if noise_data:
+        noise_data()
