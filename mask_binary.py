b24c978904bbbace373a1e9943beab045c9919b0
maxime-torre <torre.maxime@gmail.com>
2023-11-27 14:58:30 +0100

    push mask code

--- mask_binary.py
+++ mask_binary.py
@@ -1,9 +1,9 @@
 import numpy as np
 from pathlib import Path
 import pickle
-import librosa
+import librosa 
 
-def calculate_amplitude_spectrogram(audio_path):
+def calculate_amplitude_spectrogram(audio_path, N, H):
     """
     Calculer le spectrogramme d'amplitude pour un fichier audio donné.
 
@@ -12,47 +12,45 @@ def calculate_amplitude_spectrogram(audio_path):
     """
     # Charger le fichier audio
     y, sr = librosa.load(audio_path, sr=None)
+    print(len(y))
 
     # Calculer le STFT
-    D = librosa.stft(y)
+    D = librosa.stft(y, n_fft=N, hop_length=H, win_length=N, window='hann')
 
     # Convertir en amplitudes
     S = np.abs(D)
 
     return S
 
-def mask_binary(noisy_spectrogram_pkl, noise_audio_path, mask_type='soft'):
+def mask_binary(raw_data_audio, noise_audio_path, N, H, mask_type='soft'):
     """
     Appliquer un masque binaire ou souple sur un spectrogramme bruité.
 
-    :param noisy_spectrogram_pkl: Chemin vers le fichier pickle contenant le spectrogramme bruité.
+    :param raw_data_audio: Chemin vers le fichier audio sans bruit.
     :param noise_audio_path: Chemin vers le fichier audio de bruit.
     :param mask_type: Type de masque à utiliser ('soft' ou 'binary').
     :return: Spectrogramme avec masque appliqué.
     """
-    # Charger le spectrogramme bruité depuis le fichier pickle
-    with open(noisy_spectrogram_pkl, 'rb') as file:
-        noisy_spectrogram = pickle.load(file)
-
     # Charger le fichier audio de bruit et calculer son spectrogramme
-    noise_spectrogram = calculate_amplitude_spectrogram(noise_audio_path)
+    audio_spectrogram = calculate_amplitude_spectrogram(raw_data_audio, N, H)
+    noise_spectrogram = calculate_amplitude_spectrogram(noise_audio_path, N, H)
 
     if mask_type == 'binary':
         # Estimation du masque binaire
-        mask = np.abs(noisy_spectrogram) > np.abs(noise_spectrogram)
-        return mask * noisy_spectrogram
+        mask = np.abs(audio_spectrogram) > np.abs(noise_spectrogram)
+        return mask
     elif mask_type == 'soft':
         # Estimation du masque souple
-        soft_mask = np.abs(noisy_spectrogram) / (np.abs(noisy_spectrogram) + np.abs(noise_spectrogram))
+        soft_mask = np.abs(audio_spectrogram) / (np.abs(audio_spectrogram) + np.abs(noise_spectrogram))
         soft_mask = np.clip(soft_mask, 0, 1)
-        return soft_mask * noisy_spectrogram
+        return soft_mask 
     else:
         raise ValueError("Invalid mask type. Choose 'soft' or 'binary'.")
-
-
-# Exemple d'utilisation
-noise_audio_path =  Path.cwd() / "data" / "only_noise" / "6.flac"# Remplacer par le chemin réel
-noise_spectrogram = Path.cwd() / "data" / "Spectros" / "0" / "6.pkl"
-soft_mask = mask_binary(noise_spectrogram, noise_audio_path)
-
-print(soft_mask)
+    
+#test
+N = 2048
+H = 1024
+noise_audio_path =  Path.cwd() / "data" / "only_noise" / "20.flac"# Remplacer par le chemin réel
+audio_raw_data_path = Path.cwd() / "data" / "raw_data_cut" / "20.flac"
+soft_mask = mask_binary(audio_raw_data_path, noise_audio_path, N, H)
+signal_and_noise = Path.cwd() / "data" / "noised_data" / "10" / "20.flac"
